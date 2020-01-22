1cec45223be9d08736ab47e71a93707c899ab583
Victoria Li <VLi@lcs.on.ca>
2020-01-22 11:37:13 -0500

    Initial Commit

---
+++ ProjectEuler/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ProjectEuler
+//
+//  Created by Li, Muchen on 2020-01-22.
+//  Copyright © 2020 Li, Muchen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
